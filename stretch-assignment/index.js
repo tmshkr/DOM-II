fc059b6cceba3891b73385d58e91e851b330fb93
Tim Shaker <tim@tmshkr.com>
2020-02-11 15:31:49 -0800

    greensock stretch goal

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,41 +1,79 @@
 console.log("hello");
 
 const block = document.querySelectorAll(".block");
+let min = 0;
 
 block.forEach((el, i) => {
-  el.onclick = () => {
-    el.parentElement.prepend(el);
+  el.onclick = function() {
+    el.style.order = --min;
+    layout();
   };
 
-  el.positionX = 0; // initialize position to 0
+  el.x = 0; // initialize position to 0
+  el.style.order = i;
 
   el.onmousedown = function() {
-    const self = this;
-    clearInterval(self.interval);
-    self.interval = setInterval(function() {
+    clearInterval(el.interval);
+    el.interval = setInterval(function() {
       if (el.getBoundingClientRect().right >= window.innerWidth) {
-        clearInterval(self.interval);
+        clearInterval(el.interval);
       } else {
-        self.positionX += 1;
+        el.x += 1;
         requestAnimationFrame(function() {
-          el.style.transform = `translateX(${self.positionX}px)`;
+          el.style.transform = `translateX(${el.x}px)`;
         });
       }
     }, 10);
   };
 
   el.onmouseup = function() {
-    const self = this;
-    clearInterval(self.interval);
-    self.interval = setInterval(function() {
-      if (self.positionX > 0) {
-        self.positionX -= 1;
-        requestAnimationFrame(function() {
-          el.style.transform = `translateX(${self.positionX}px)`;
-        });
-      } else {
-        clearInterval(self.interval);
-      }
-    }, 10);
+    clearInterval(el.interval);
+    layout();
   };
 });
+
+// https://codepen.io/osublake/pen/gaQNLK?editors=0010
+var nodes = block;
+var boxes = [];
+
+for (var i = 0; i < nodes.length; i++) {
+  var node = nodes[i];
+
+  // Initialize transforms on node
+  TweenLite.set(node, { x: 0 });
+
+  boxes[i] = {
+    transform: node._gsTransform,
+    x: node.x,
+    y: node.offsetTop,
+    node
+  };
+}
+
+function layout() {
+  for (var i = 0; i < nodes.length; i++) {
+    var box = boxes[i];
+
+    var lastX = box.x;
+    var lastY = box.y;
+
+    box.x = box.node.offsetLeft;
+    box.y = box.node.offsetTop;
+
+    // Continue if box hasn't moved
+    if (lastX === box.x && lastY === box.y) continue;
+
+    // Reversed delta values taking into account current transforms
+    var x = nodes[i].x;
+    var y = box.transform.y + lastY - box.y;
+
+    // Tween to 0 to remove the transforms
+    TweenLite.fromTo(
+      box.node,
+      0.5,
+      { x, y },
+      { x: 0, y: 0, ease: Power1.easeInOut }
+    );
+    nodes[i].x = 0;
+  }
+}
