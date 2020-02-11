3b890c051856ec4cfc511792072f816a27ba35a6
Tim Shaker <tim@tmshkr.com>
2020-02-10 20:11:52 -0800

    add event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,50 @@
-// Your code goes here
+const funBus = document.querySelector("header.intro img");
+
+funBus.addEventListener("mouseover", e => {
+  e.target.style.transform = "scaleX(-1)";
+});
+funBus.addEventListener("mouseleave", e => {
+  e.target.style.transform = null;
+});
+funBus.addEventListener("mousedown", e => {
+  e.target.style.filter = "invert(1)";
+});
+funBus.addEventListener("mouseup", e => {
+  e.target.style.filter = null;
+});
+
+document.querySelectorAll("p, h2, h4").forEach(el => {
+  el.onmouseover = e => {
+    e.target.style.color = "green";
+  };
+  el.onmouseleave = e => {
+    e.target.style.color = null;
+  };
+});
+
+window.addEventListener("load", () => {
+  alert("Welcome to Fun Bus");
+});
+
+const header = document.querySelector("header.main-navigation");
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault();
+    e.stopPropagation();
+  });
+});
+
+header.addEventListener("click", e => {
+  header.style.background = "red";
+});
+header.addEventListener("mouseleave", e => {
+  header.style.background = null;
+});
+
+document.querySelectorAll("div.destination div.btn").forEach(el => {
+  el.onclick = e => {
+    e.target.parentElement.style.border = "3px solid blue";
+  };
+});
